7387dd92ae0e02ee841b4a72c402bafbc8a0b1b0
vinayak2882 <vinayak.patrut@gmail.com>
2022-04-29 17:36:57 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import RestaurantDetails from "./components/RestaurantDetails";
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+       <RestaurantDetails />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/RestaurantDetails.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import Buttons from "./Buttons";
+import Cards from "./Cards";
+import Form from "./Form";
+// import "./style.css"
+
+const RestaurantDetails = () => {
+   const [data, setData] = useState([]);
+
+   // To fetch and update the data
+
+   const getData = async () => {
+      let fetched = await fetch("http://localhost:8080/data");
+      fetched = await fetched.json();
+      setData([...fetched, ...data]);
+      return fetched;
+   };
+
+   useEffect(() => {
+      getData();
+   }, []);
+
+   return (
+      <div>
+         <Form />
+         <Buttons getData={getData} setData={setData} data={data} />
+         <Cards data={data} />
+      </div>
+   );
+};
+
+export default RestaurantDetails;
